af84ffb0a809602eb7b4b97d27629fa573a05499
Lucia <doitelu@gmail.com>
2020-12-08 21:46:45 -0800

    Add controls to the gallery

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -1,42 +1,41 @@
 import cn from 'classnames'
 import { useEffect, useState } from 'react'
+import { useInterval } from '../../services'
 import shuffle from 'lodash/shuffle'
 import css from './Gallery.less'
 
 export default function Gallery({ files = [], interval = 1, setStarted, started }) {
     const [shuffledFiles, setShuffledFiles] = useState([])
     const [activeIndex, setActiveIndex] = useState(0)
-    const [intervalId, setIntervalId] = useState()
+    const [paused, setPaused] = useState(false)
+    const stopGallery = () => {
+        setPaused(false)
+        setStarted(false)
+        setActiveIndex(0)
+    }
+    const moveBy = (delta) => {
+        const newIndex = activeIndex + delta
+
+        if (newIndex < 0 || newIndex >= files.length) {
+            stopGallery()
+        } else {
+            setActiveIndex(newIndex)
+        }
+    }
 
     useEffect(() => {
         setShuffledFiles(shuffle(files))
     }, [files])
 
-    useEffect(() => {
-        if (!started || !files.length) return
-        if (intervalId) clearInterval(intervalId)
+    useInterval(function tick() {
+        if (paused || !started || !files.length) return
 
-        const id = setInterval(() => {
-            let stop = false
-
-            setActiveIndex((activeIndex) => {
-                if (activeIndex < files.length - 1) {
-                    return activeIndex + 1
-                } else {
-                    stop = true
-                    return 0
-                }
-            })
-
-            if (stop) {
-                setStarted(false)
-                clearInterval(id)
-            }
-        }, interval * 1000)
-
-        setIntervalId(id)
-        return () => clearInterval(id)
-    }, [started])
+        if (activeIndex < files.length - 1) {
+            setActiveIndex(activeIndex + 1)
+        } else {
+            stopGallery()
+        }
+    }, interval * 1000) // TODO: use this function as second ticking by, so that pause work properly, not skipping rounds
 
     return (
         <section className={cn(css.gallery, { [css.started]: started })}>
@@ -46,12 +45,19 @@ export default function Gallery({ files = [], interval = 1, setStarted, started 
 
             {shuffledFiles.map((file, i) => (
                 <div
-                    className={cn(css.imageContainer, { [css.active]: i === activeIndex })}
                     key={i}
+                    className={cn(css.imageContainer, { [css.active]: i === activeIndex })}
                 >
                     <img src={file.preview} />
                 </div>
             ))}
+
+            <div className={css.controls}>
+                <button onClick={() => moveBy(-1)}>{'<'}</button>
+                <button onClick={() => setPaused(!paused)}>{paused ? 'Play' : 'Pause'}</button>
+                <button onClick={() => setStarted(false)}>Stop</button>
+                <button onClick={() => moveBy(1)}>{'>'}</button>
+            </div>
         </section>
     )
 }
