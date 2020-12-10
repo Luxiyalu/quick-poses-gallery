2a78e18486c513e2ae67cf381753e96a334e6934
Lucia <doitelu@gmail.com>
2020-12-09 20:30:10 -0800

    Add google analysis

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import Head from 'next/head'
 import '../styles/globals.less'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const script = `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date()); gtag('config', 'G-3R3EJQXDW2');`
+
+    return (
+        <>
+            <Head>
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-3R3EJQXDW2"
+                ></script>
+                <script dangerouslySetInnerHTML={{ __html: script }} />
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
